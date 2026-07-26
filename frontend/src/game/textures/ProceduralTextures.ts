b2e1c01b6e9ef8a6730d991a9827663cc672a485
Dani-8 <daniyalw6190@gmail.com>
2026-07-26 23:41:36 +0500

    Updates

--- frontend/src/game/textures/ProceduralTextures.ts
+++ frontend/src/game/textures/ProceduralTextures.ts
@@ -73,7 +73,7 @@ export class ProceduralTextures {
     const ctx = canvas.getContext();
 
     if (blur) {
-      const grad = ctx.createRadialGradient(size/2, size/2, 0, size/2, size/2, size/2);
+      const grad = ctx.createRadialGradient(size / 2, size / 2, 0, size / 2, size / 2, size / 2);
       grad.addColorStop(0, colorStr);
       grad.addColorStop(1, 'rgba(255,255,255,0)');
       ctx.fillStyle = grad;
@@ -82,7 +82,7 @@ export class ProceduralTextures {
     }
 
     ctx.beginPath();
-    ctx.arc(size/2, size/2, size/2, 0, Math.PI * 2);
+    ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
     ctx.fill();
     canvas.refresh();
   }
