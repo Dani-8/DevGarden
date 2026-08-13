62354316046b98db54bc376f430e2781d14bc4e2
Dani-8 <daniyalw6190@gmail.com>
2026-08-13 23:15:38 +0500

    Updates

--- frontend/src/game/cafe/CafePropsManager.ts
+++ frontend/src/game/cafe/CafePropsManager.ts
@@ -156,6 +156,7 @@ export class CafePropsManager {
         spawnPot(276, 135);
         spawnPot(618, 135);
         spawnPot(100, 125); // Next to bookshelf
+        spawnPot(20, 125);
 
         // Left Side of Center Garden Planter: Alternating Lamps & Pots snug against planter (x: 394)
         spawnLamp(398, 230); // Lamp 1
