c5175641bdefc12710b90e11f9220ac5e1b485cb
Dani-8 <daniyalw6190@gmail.com>
2026-09-18 20:11:55 +0500

    Updates

--- frontend/src/game/scenes/cafe/CafeMovementManager.ts
+++ frontend/src/game/scenes/cafe/CafeMovementManager.ts
@@ -4,51 +4,51 @@ import { CafeBaristaManager } from '../../cafe/CafeBaristaManager';
 import { CafeInteractionManager } from './CafeInteractionManager';
 
 export class CafeMovementManager {
-  private lastMoveSent: number = 0;
-  private lastX: number = 0;
-  private lastY: number = 0;
-  private lastAnim: string = 'idle_down';
+    private lastMoveSent: number = 0;
+    private lastX: number = 0;
+    private lastY: number = 0;
+    private lastAnim: string = 'idle_down';
 
-  constructor(
-    private scene: Phaser.Scene,
-    private socket: any,
-    private selfPlayer: PlayerState | null,
-    private interactionManager: CafeInteractionManager,
-    private baristaManager: CafeBaristaManager,
-    private otherPlayers: Map<string, Phaser.GameObjects.Container>,
-    private showcasePos?: { x: number; y: number }
-  ) {}
+    constructor(
+        private scene: Phaser.Scene,
+        private socket: any,
+        private selfPlayer: PlayerState | null,
+        private interactionManager: CafeInteractionManager,
+        private baristaManager: CafeBaristaManager,
+        private otherPlayers: Map<string, Phaser.GameObjects.Container>,
+        private showcasePos?: { x: number; y: number }
+    ) { }
 
-  public handleUpdate(
-    playerContainer: Phaser.GameObjects.Container | null,
-    playerSprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | null,
-    cursors: Phaser.Types.Input.Keyboard.CursorKeys,
-    wasd: {
-      W: Phaser.Input.Keyboard.Key;
-      A: Phaser.Input.Keyboard.Key;
-      S: Phaser.Input.Keyboard.Key;
-      D: Phaser.Input.Keyboard.Key;
-    }
-  ) {
-    if (
-      this.interactionManager.isTransitioning ||
-      !playerContainer ||
-      !playerSprite ||
-      !cursors ||
-      !wasd
+    public handleUpdate(
+        playerContainer: Phaser.GameObjects.Container | null,
+        playerSprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody | null,
+        cursors: Phaser.Types.Input.Keyboard.CursorKeys,
+        wasd: {
+            W: Phaser.Input.Keyboard.Key;
+            A: Phaser.Input.Keyboard.Key;
+            S: Phaser.Input.Keyboard.Key;
+            D: Phaser.Input.Keyboard.Key;
+        }
     ) {
-      return;
-    }
+        if (
+            this.interactionManager.isTransitioning ||
+            !playerContainer ||
+            !playerSprite ||
+            !cursors ||
+            !wasd
+        ) {
+            return;
+        }
 
-    const speed = 110;
-    let vx = 0;
-    let vy = 0;
-    let animKey = 'idle_down';
+        const speed = 110;
+        let vx = 0;
+        let vy = 0;
+        let animKey = 'idle_down';
 
-    if (cursors.left.isDown || wasd.A.isDown) {
-      vx = -speed;
-      animKey = 'walk_left';
-    } else if (cursors.right.isDown || wasd.D.isDown) {
-      vx = speed;
-      animKey = 'walk_right';
-    }
+        if (cursors.left.isDown || wasd.A.isDown) {
+            vx = -speed;
+            animKey = 'walk_left';
+        } else if (cursors.right.isDown || wasd.D.isDown) {
+            vx = speed;
+            animKey = 'walk_right';
+        }
