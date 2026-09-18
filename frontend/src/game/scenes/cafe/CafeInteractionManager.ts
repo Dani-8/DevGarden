092573f47761a0329f40ee7503361e2da003fdf4
Dani-8 <daniyalw6190@gmail.com>
2026-09-19 00:51:12 +0500

    Updates

--- frontend/src/game/scenes/cafe/CafeInteractionManager.ts
+++ frontend/src/game/scenes/cafe/CafeInteractionManager.ts
@@ -3,22 +3,22 @@ import { CafeChair } from '../../cafe/CafePropsManager';
 import { PlayerManager } from '../PlayerManager';
 
 export class CafeInteractionManager {
-  public promptText!: Phaser.GameObjects.Text;
-  public sitPromptText!: Phaser.GameObjects.Text;
-  public showcasePromptText!: Phaser.GameObjects.Text;
-  public collabPromptText!: Phaser.GameObjects.Text;
-  public isSitting: boolean = false;
-  public isTransitioning: boolean = false;
+    public promptText!: Phaser.GameObjects.Text;
+    public sitPromptText!: Phaser.GameObjects.Text;
+    public showcasePromptText!: Phaser.GameObjects.Text;
+    public collabPromptText!: Phaser.GameObjects.Text;
+    public isSitting: boolean = false;
+    public isTransitioning: boolean = false;
 
-  constructor(
-    private scene: Phaser.Scene,
-    private playerManager: PlayerManager,
-    private cafeChairs: CafeChair[],
-    private eKey: Phaser.Input.Keyboard.Key,
-    private oKey: Phaser.Input.Keyboard.Key,
-    private onExitToGarden: () => void,
-    private onOpenShowcase?: () => void,
-    private onOpenCollab?: () => void
-  ) {
-    this.createPrompts();
-  }
+    constructor(
+        private scene: Phaser.Scene,
+        private playerManager: PlayerManager,
+        private cafeChairs: CafeChair[],
+        private eKey: Phaser.Input.Keyboard.Key,
+        private oKey: Phaser.Input.Keyboard.Key,
+        private onExitToGarden: () => void,
+        private onOpenShowcase?: () => void,
+        private onOpenCollab?: () => void
+    ) {
+        this.createPrompts();
+    }
